8242e5205ce19960f57cac99b88b0ae026ccf6ab
Aastik Nayyar <aastiknayyar13@gmail.com>
2026-02-24 23:51:37 +0530

    added frontend and backend urls

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,22 @@ const { generateSummary } = require("./services/geminiService");
 const { extractTextFromPDF } = require("./utils/pdfExtractor");
 
 const app = express();
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173", 
+  "https://earnings-call-reasearch.vercel.app"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+}));
 app.use(express.json());
 
 const upload = multer({ storage: multer.memoryStorage() });
